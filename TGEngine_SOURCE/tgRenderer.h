197b1e07d779f500b236e7ea53b6afbe373ae30e
pfsimo1442 <pfsimo1442@gmail.com>
2025-09-04 11:06:50 +0900

    added triangle shader tested draw triangle

--- TGEngine_SOURCE/tgRenderer.h
+++ TGEngine_SOURCE/tgRenderer.h
@@ -1,8 +1,25 @@
 #pragma once
 #include "tgCamera.h"
+#include "tgGraphicDevice_DX11.h"
 
 namespace tg::renderer
 {
 	extern Camera* mainCamera;
+
+	struct Vertex
+	{
+		Vector3 pos;
+		Vector4 color;
+	};
+
+	extern Vertex vertexes[3];
+	extern ID3D11Buffer* vertexBuffer;
+
+	extern ID3DBlob* vsBlob;
+	extern ID3D11VertexShader* vsShader;
+
+	extern ID3DBlob* psBlob;
+	extern ID3D11PixelShader* psShader;
+	extern ID3D11InputLayout* inputLayouts;
 }
 
